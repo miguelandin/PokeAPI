c716067d520c7f5de3167273d7996c1d5d395916
mike <miguel.cabrera.landin@gmail.com>
2025-09-30 12:42:50 +0200

    ahora funciona al menos

--- pokeAPI.js
+++ pokeAPI.js
@@ -1,18 +1,28 @@
-const pokeList = document.getElementById('pokemon-ul');
+window.addEventListener('load', () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=9')
+    .then(result => result.json()) // convierte las respuesta a un json
+    .then(data => cargarDatos(data.results)); // llama la cargarDato como array
+})
 
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error en la red');
-        }
-        return response.json();
+function cargarDatos(data) {
+    console.log(data);
+    data.forEach(pokemon => {
+        const li = document.createElement('li'); // crea un nuevo li
+        li.innerHTML = pokemon.name; // mete dentro de <li></li> los nombres
+        li.addEventListener('click', () => mosrtar(pokemon.url));
+        // añade el primer ul y añade como hijo el li actual
+        document.querySelector('ul').appendChild(li);
     })
+}
+
+function mosrtar(url) {
+    fetch(url).then(result => result.json())
     .then(data => {
-        data.results.forEach(pokemon => {
-            const li = document.createElement('li');
-            li.textContent = pokemon.name; // Nombre del Pokémon
-            pokeList.appendChild(li); // Usa pokeList aquí
-        });
-    })
-    .catch(error => console.error('Error al cargar los datos: ', error));
+        const nombre = data.name;
+        const altura = data.height;
+        const peso = data.weight;
 
+        const valores = `Nombre: ${nombre}, Altura: ${altura}, Peso: ${peso}`;
+        document.querySelector('dialog').innerHTML(valores);
+    })
+}
